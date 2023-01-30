700d332c0a082ba10c244f58960ad4bb9a565f0a
Jesus Raso <34660217+jesusinbaney@users.noreply.github.com>
2023-01-30 01:59:22 +0100

    cambios

--- basic/controllers/LibrostraductoresController.php
+++ basic/controllers/LibrostraductoresController.php
@@ -1,12 +1,28 @@
 <?php
 
 namespace app\controllers;
+use app\models\Libros;
+use app\models\LibrosSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\data\ActiveDataProvider;
+use Yii;
 
-class LibrostraductoresController extends \yii\web\Controller
+class LibrosilustradoresController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        //$searchModel = new LibrosSearch();
+        $dataProvider = new ActiveDataProvider([
+            'query' => Libros::find()->joinWith('traductores'),
+        ]);
+        //$dataProvider = $searchModel->getlibrosIlustradores(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+          //  'searchModel' => $searchModel,
+          'dataProvider' => $dataProvider,
+
+        ]);
     }
 
 }
