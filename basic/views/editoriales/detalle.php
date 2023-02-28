2bfea7d3d68d3fca8cda0b393a30eee9bac64e7e
davidgarcialazaro <davidgarcialazaro@usal.es>
2023-02-28 15:18:55 +0100

    Correccion ver detalles editoriales
    Correccion ver detalles editoriales

--- basic/views/editoriales/detalle.php
+++ basic/views/editoriales/detalle.php
@@ -14,14 +14,9 @@ use yii\helpers\Url;
         <?php
 		echo Html::a(Yii::t('app', $editorial->nombre), ['libros_editorial', 'id'=>$editorial->id], ['class' => 'btn btn-outline-secondary']);
 		?>
+		<br/>
         <p><?= Html::encode($editorial->descripcion) ?></p>
         <p>Url: <a href="<?= Html::encode($editorial->url) ?>"><?= Html::encode($editorial->url) ?></a></p>
-			<?php
-			// Botón para ver la ficha resumida de la editorial seleccionada
-			?>
-			<a href="<?= Url::toRoute(['/editoriales/detalle', 'id' => $editorial->id, 'class' => 'btn btn-primary']); ?>" style=" background-color: #4CAF50;  border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;">
-				Ver Detalles
-			</a>
         <hr>
 	</div>
 </div>
