d991b832828b491605d67d14eeabdd01379ea96d
SaraCostaT <saracostat@usal.es>
2023-03-12 19:44:07 +0100

    Buscador a-z arreglado en autores

--- basic/controllers/AutoresController.php
+++ basic/controllers/AutoresController.php
@@ -182,5 +182,27 @@ class AutoresController extends Controller
         "dataProvider" => $dataProvider,
     ));
 	}
+
+    public function actionLetrafilter($filtroLetra)
+    {
+		$searchModel = new AutoresSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+		$pagination = new Pagination([
+			'defaultPageSize' => Configuraciones::getConfiguracion("numero_lineas_pagina"),
+			'totalCount' => $dataProvider->query->count(),
+		]);
+		
+        $autores=$dataProvider->query->where(['like','nombre',$filtroLetra.'%',false])->offset($pagination->offset)->limit($pagination->limit)->all();
+  
+		$dataLetras = $searchModel->search([]);
+		$letra=$dataLetras->query->select(['SUBSTRING(nombre,1,1) as letra'])->distinct()->orderBy(['letra'=> SORT_ASC])->asArray()->all();
+		
+		return $this->render('index', [
+            'searchModel' => $searchModel,
+            'pagination' => $pagination,
+            'autores' => $autores,
+            'letra' => $letra,
+        ]);
+    }// actionLetra
 	
 }
